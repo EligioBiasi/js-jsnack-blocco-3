1f29765ef493f5d53fa70ede332702abbca13024
EligioBiasi <biasi.eligio@edufaimm.it>
2023-06-15 12:46:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import General from '../src/assets/general.scss'
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>
