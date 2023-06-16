49d7b38a12d5a64db6fd25902e86553c8e193734
EligioBiasi <biasi.eligio@edufaimm.it>
2023-06-16 12:30:36 +0200

    attempt

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import General from '../src/assets/general.scss'
+<script>
+import general from '../src/assets/general.scss'
+import HeaderApp from './components/HeaderApp.vue'
+
+export default{
+    components:{
+        HeaderApp,
+    }
+}
 </script>
 
 <template>
-  
+  <HeaderApp />
 </template>
 
 <style>
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="link in LinksContainer">
+
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import NavLink from './NavLink.vue'
+
+export default {
+    components:{
+        NavLink,
+    },
+    data(){
+        return{
+            LinksContainer:[
+                {
+                    link:1,
+                },
+                {
+                    link:2,
+                },
+                {
+                    link:3,
+                },
+                {
+                    link:4,
+                },
+                {
+                    link:5,
+                },
+            ]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
